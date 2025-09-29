568102af3826127e2141957b7db1cce694681de3
ALIERX <a.khamraev@gmail.com>
2025-09-29 21:49:43 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
-
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './ui/App.jsx'
-import './styles/index.css'
-
-createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-)
+ import React from 'react'
+ import { createRoot } from 'react-dom/client'
+ import { BrowserRouter } from 'react-router-dom'
+ import App from './ui/App.jsx'
++import ErrorBoundary from './ui/ErrorBoundary.jsx'
+ // (se hai il SoundProvider, lascialo)
+ 
+ createRoot(document.getElementById('root')).render(
+   <React.StrictMode>
+     <BrowserRouter>
+-      <App />
++      <ErrorBoundary>
++        <App />
++      </ErrorBoundary>
+     </BrowserRouter>
+   </React.StrictMode>
+ )
